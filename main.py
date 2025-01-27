2d0da80b27f34aaa5665be6fd30f88b113ddc884
Aleksei-Kutuzov <allekskutuzov@gmail.com>
2025-01-27 11:32:53 +0300

    .

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import threading
 
 from flask import Flask, render_template, request, session
 from turbo_flask import Turbo
+from flask import request, render_template
+import threading
 
 import promts
 from pix2pix import PIX2PIX
@@ -27,15 +29,17 @@ class YouNeiroProfFrame(Flask):
     def pix2pix_model_query(self, model: PIX2PIX, filename: str, profession: str, id: uuid.uuid4):
         promt = promts.promts[profession]
         with app.app_context():
-            self.turbo.push(self.turbo.replace(render_template("preloader.html", id=id), "load"))
+            self.turbo.push(self.turbo.replace(render_template("preloader.html", id=str(id)), str(id)))
         PIX2PIX.model_query(model, filename, promt)
-
+        print("Q RETURN")
         with app.app_context():
+            print()
+            print(str(id))
             self.turbo.push(self.turbo.replace(f"<img src='static/generates/{filename}' class='card-img-top' alt='...' id='{str(id)}'>", str(id)))
 
 
 app = YouNeiroProfFrame(__name__)
-os.environ["REPLICATE_API_TOKEN"] = "r8_6VXzvRUEZpSb9LB7RFBrllO4jDKUmT83XJqsh"
+os.environ["REPLICATE_API_TOKEN"] = "¯\_(ツ)_/¯"
 
 model = PIX2PIX()
 
@@ -58,9 +62,6 @@ def choose_profession():
     loading_div_id = f"load-{user_id}"  # Уникальный ID для обновлений
     return render_template('profession-choose.html', profession_list=list(promts.promts.keys()), allowedTypes=list(app.config['ALLOWED_EXTENSIONS']), id=loading_div_id)
 
-from flask import request, render_template
-import threading
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'profession-choose-file' not in request.files:
@@ -75,6 +76,7 @@ def upload_file():
         file.save(f"uploads/" + filename)
         # Запуск фоновой задачи
         profession = request.form.get("profession-option")
+        print(profession)
         if profession:
             threading.Thread(target=app.pix2pix_model_query, args=(model, filename, profession, loading_div_id)).start()
         else:
